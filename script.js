fd9cbb5562681d2c3d3a01b6f173efa9226b3385
Elian Garcia <albertbrooklynny123@gmail.com>
2020-08-03 21:04:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,1 @@
+document.getElementById("button").addEventListener("click", function(){ alert("Hi there, have you heard of the wonderful game of chess?? Im a strong advocate of this wonderful sport"); });
